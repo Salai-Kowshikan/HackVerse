0fa1fed693e7a8163e8c96688dcb622dde65cae1
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-01-12 14:34:36 +0530

    [Backend] commit last changes

--- Backend/services/Textract.py
+++ Backend/services/Textract.py
@@ -52,20 +52,9 @@ def extract_text(result):
 
 def crop_largest_rectangle(image_path):
     image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    edged = cv2.Canny(blurred, 50, 150)
-
-    contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    largest_contour = max(contours, key=cv2.contourArea)
-
-    x, y, w, h = cv2.boundingRect(largest_contour)
-    cropped_image = image[y:y+h, x:x+w]
-
-    _, buffer = cv2.imencode('.jpg', cropped_image)
-    cropped_image_base64 = base64.b64encode(buffer).decode('utf-8')
-
-    return cropped_image_base64
+    _, buffer = cv2.imencode('.jpg', image)
+    image_base64 = base64.b64encode(buffer).decode('utf-8')
+    return image_base64
 
 if __name__ == "__main__":
     image_path = 'Backend\\test3.jpg'
