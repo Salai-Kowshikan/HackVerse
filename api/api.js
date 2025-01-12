af8fa05e68d8c5d3ccde2ee3e48679efb3ec7800
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-01-12 08:08:16 +0530

    [Dev] Trying to deploy

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const db = axios.create({
-  baseURL: 'http://192.168.154.103:5000',
+  baseURL: 'http://192.168.154.196:5000',
   headers: {
     'Content-Type': 'application/json',
   },
