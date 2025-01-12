59d4374f895c2ee42ce0475603bd581b20dfc982
Sri Dharani R <sridharanirajendran26@gmail.com>
2025-01-12 09:47:42 +0530

    [Frontend] Icon and splash image changed

--- api/api.js
+++ api/api.js
@@ -3,11 +3,7 @@ import axios from 'axios';
 const apiUrl = process.env.EXPO_PUBLIC_API
 
 const db = axios.create({
-<<<<<<< Updated upstream
-  baseURL: apiUrl,
-=======
   baseURL: 'https://hackverse-79o4.onrender.com',
->>>>>>> Stashed changes
   headers: {
     'Content-Type': 'application/json',
   },
